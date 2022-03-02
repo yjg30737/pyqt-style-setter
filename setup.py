2c2658fe0f5b3b9a233cd8885150eea0c79c6d7a
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-02 10:37:48 +0900

    Fisrt commit from local

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyqt-style-setter',
+    version='0.0.1',
+    author='Jung Gyu Yoon',
+    author_email='yjg30737@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    description='PyQt Style Setter',
+    url='https://github.com/yjg30737/pyqt-style-setter.git',
+    install_requires=[
+        'PyQt5>=5.8',
+        'pyqt-dark-gray-theme @ git+https://git@github.com/yjg30737/pyqt-dark-gray-theme.git@main'
+    ]
+)
