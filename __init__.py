1701a67bacac070b9c96d0df0e684d8326f93004
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-02 10:34:45 +0900

    First commit from local

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .styleSetter import StyleSetter
---
+++ styleSetter.py
@@ -0,0 +1,18 @@
+from PyQt5.QtWidgets import QWidget, QAbstractButton
+from pyqt_dark_gray_theme import *
+
+
+class StyleSetter:
+    @staticmethod
+    def setWindowStyle(main_window: QWidget):
+        main_window.setStyleSheet(getThemeStyle())  # theme
+        btns = main_window.findChildren(QAbstractButton)  # buttons
+        for btn in btns:
+            # check if text exists
+            if btn.text().strip() == '':
+                btn.setStyleSheet(getIconButtonStyle())  # no text - icon button style
+            else:
+                btn.setStyleSheet(getIconTextButtonStyle())  # text - icon-text button style
+        menu_bar = main_window.menuBar()  # menu bar
+        menu_bar_style = getMenuBarStyle(menu_bar)
+        menu_bar.setStyleSheet(menu_bar_style)
