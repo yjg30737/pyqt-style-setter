2b2e34f8bacbddb040cb6b2e653eb565e60e2294
Jung Gyu Yoon <yjg30737@gmail.com>
2022-04-11 15:28:06 +0900

    Add argument (theme: str = 'dark')

--- pyqt_style_setter/styleSetter.py
+++ pyqt_style_setter/styleSetter.py
@@ -5,48 +5,49 @@ from pyqt_svg_icon_pushbutton import SvgIconPushButton
 
 class StyleSetter:
     @staticmethod
-    def setWindowStyle(main_window: QWidget, exclude_type_lst: list = []):
-        if isinstance(main_window, QMainWindow):
-            main_window.setStyleSheet(getThemeStyle())  # theme
-        elif isinstance(main_window, QDialog):
-            main_window.setStyleSheet(getThemeStyle())  # theme
-        else:
-            main_window.setObjectName('widget')
-            main_window.setStyleSheet(getMainWidgetStyle() + getThemeStyle())
+    def setWindowStyle(main_window: QWidget, theme: str = 'dark', exclude_type_lst: list = []):
+        if theme == 'dark':
+            if isinstance(main_window, QMainWindow):
+                main_window.setStyleSheet(getThemeStyle())  # theme
+            elif isinstance(main_window, QDialog):
+                main_window.setStyleSheet(getThemeStyle())  # theme
+            else:
+                main_window.setObjectName('widget')
+                main_window.setStyleSheet(getMainWidgetStyle() + getThemeStyle())
 
-        # button
-        def setButtonStyle(main_window):
-            btns = main_window.findChildren(QAbstractButton)
-            for btn in btns:
-                # check if text exists
-                if btn.text().strip() == '':
-                    # if button type is SvgIconPushButton, let it maintain its own style
-                    if isinstance(btn, SvgIconPushButton):
-                        pass
+            # button
+            def setButtonStyle(main_window):
+                btns = main_window.findChildren(QAbstractButton)
+                for btn in btns:
+                    # check if text exists
+                    if btn.text().strip() == '':
+                        # if button type is SvgIconPushButton, let it maintain its own style
+                        if isinstance(btn, SvgIconPushButton):
+                            pass
+                        else:
+                            btn.setStyleSheet(getIconButtonStyle())  # no text - icon button style
                     else:
-                        btn.setStyleSheet(getIconButtonStyle())  # no text - icon button style
-                else:
-                    # if button type is SvgIconPushButton, let it maintain its own style
-                    if isinstance(btn, SvgIconPushButton):
-                        pass
-                    else:
-                        btn.setStyleSheet(getIconTextButtonStyle())  # text - icon-text button style
+                        # if button type is SvgIconPushButton, let it maintain its own style
+                        if isinstance(btn, SvgIconPushButton):
+                            pass
+                        else:
+                            btn.setStyleSheet(getIconTextButtonStyle())  # text - icon-text button style
 
-        # check exclusion of QAbstractButton
-        if QAbstractButton in exclude_type_lst:
-            pass
-        else:
-            setButtonStyle(main_window)
+            # check exclusion of QAbstractButton
+            if QAbstractButton in exclude_type_lst:
+                pass
+            else:
+                setButtonStyle(main_window)
 
-        # todo check exclusion of other types
-        if len(exclude_type_lst) > 0:
-            for _type in exclude_type_lst:
-                widgets = main_window.findChildren(_type)
-                if isinstance(_type, QAbstractButton):
-                    print(_type)
+            # todo check exclusion of other types
+            if len(exclude_type_lst) > 0:
+                for _type in exclude_type_lst:
+                    widgets = main_window.findChildren(_type)
+                    if isinstance(_type, QAbstractButton):
+                        print(_type)
 
-        if isinstance(main_window, QMainWindow):
-            menu_bar = main_window.menuBar()  # menu bar
-            menu_bar_style = getMenuBarStyle(menu_bar)
-            menu_bar.setStyleSheet(menu_bar_style)
+            if isinstance(main_window, QMainWindow):
+                menu_bar = main_window.menuBar()  # menu bar
+                menu_bar_style = getMenuBarStyle(menu_bar)
+                menu_bar.setStyleSheet(menu_bar_style)
 
