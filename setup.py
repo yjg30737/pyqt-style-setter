565fff89bc8bd023b344ab82f8e19db1253fd5e2
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-12 10:57:20 +0900

    Update url of each packages

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setup(
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
-    description='PyQt Style Setter',
+    description='PyQt style setter',
     url='https://github.com/yjg30737/pyqt-style-setter.git',
     install_requires=[
         'PyQt5>=5.8',
-        'pyqt-dark-gray-theme @ git+https://git@github.com/yjg30737/pyqt-dark-gray-theme.git@main',
-        'pyqt-svg-icon-pushbutton @ git+https://git@github.com/yjg30737/pyqt-svg-icon-pushbutton.git@main'
+        'pyqt-dark-gray-theme>=0.0.1',
+        'pyqt-svg-icon-pushbutton>=0.0.1'
     ]
 )
