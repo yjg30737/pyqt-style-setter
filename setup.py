94b9c3be53edb95b58c8aff7f7178683485fb936
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-12 11:10:22 +0900

    Update setup.py to add pypi description

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-style-setter',
-    version='0.0.1',
+    version='0.0.11',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
     description='PyQt style setter',
     url='https://github.com/yjg30737/pyqt-style-setter.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8',
         'pyqt-dark-gray-theme>=0.0.1',
